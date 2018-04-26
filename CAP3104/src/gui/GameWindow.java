2fa5ac4416dd6225ad99855c1da2b9ce85a2199a
theGANOUSH <babaganoush45@outlook.com>
2018-04-25 23:20:33 -0400

    Added Images and main game window.

---
+++ CAP3104/src/gui/GameWindow.java
@@ -0,0 +1,46 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+
+public class GameWindow extends JFrame {
+
+	private static final long serialVersionUID = 7058806678771843480L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GameWindow frame = new GameWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GameWindow() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(GameWindow.class.getResource("/gui/images/TheTab_KGrgb_300ppi.png")));
+		setTitle("CAP3104 - Chess");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
